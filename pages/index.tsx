e0a3a2ed0122e8b008dd1fe87bab952afc8d4fd2
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-25 07:44:16 -0500

    eliminar fetching de playlists desde el api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import { Navbar } from '@/components/Navbar';
 import { PlaylistContainer } from '@/components/PlaylistContainer';
 import { Sidebar } from '@/components/Sidebar';
-import useSWR from 'swr';
+import { playlists } from '@/datos';
+// import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home = () => {
-  const { data, isLoading } = useSWR('/api/data', fetcher);
+  // const { data, isLoading } = useSWR(
+  //   'https://clone-spotify-lw.vercel.app/api/data',
+  //   fetcher
+  // );
 
-  if (isLoading) return <div>Loading...</div>;
+  // if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className='bg-black text-white w-full h-screen flex'>
@@ -17,8 +21,11 @@ const Home = () => {
         <div className='container-card flex flex-col h-full'>
           <Navbar />
           <div className='bg-gradient-to-t from-spotify-light-black to-spotify-light-black-3 px-4 h-full flex flex-col gap-7'>
-            <PlaylistContainer data={data.playlists} name={'Spotify Playlists'} />
-            <PlaylistContainer data={data.focus} name={'Focus'} />
+            <PlaylistContainer
+              data={playlists.playlists}
+              name={'Spotify Playlists'}
+            />
+            <PlaylistContainer data={playlists.focus} name={'Focus'} />
           </div>
         </div>
       </main>
